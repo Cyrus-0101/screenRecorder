35d7b6de2cb534b68d0a9389a881cbd6faa6bc79
Toxic254 <cyrus_g@outlook.com>
2020-03-11 20:25:28 +0300

    Added Screen Recording Function, Raw Data Processing Functionalities, data buffering for storage and playback.

--- src/render.js
+++ src/render.js
@@ -21,8 +21,60 @@ async function getVideoSources() {
         inputSources.map(source => {
             return {
                 label: source.name,
-                click: () => selectSource()
-            }
+                click: () => selectSource(source)
+            };
         })
     );
+
+    videoOptionsMenu.popup();
 };
+
+//A media recorder intance that lets you capture live footage
+let mediaRecorder;
+const recordedChunks = [];
+
+//Selecting the VideoSource for the screen to record
+async function selectSource(source) {
+
+    videoSelectBtn.innerText = source.name;
+
+    const constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    }
+    //Creating A Stream
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+    //Preview the source in a video
+    videoElement.srcObject = stream;
+    videoElement.play();
+
+    //Creating a Media Recorder
+    const options = { mimeType: 'video/webm: codecs=vp9' };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    //Registering Event Handlers
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+};
+
+//Capturing all Recorded Chunks
+function handleDataAvailable(e) {
+    console.log("Video Data Available");
+    recordedChunks.push(e.data);
+};
+
+//Converts data to blob because blob is a data structure to handle raw data such as videos
+//Save the video file on stop
+async function handleStop(e) {
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm; codecs=vp9' 
+    });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+}
